728b03d199f3b1b61bcdb40b3a0c7c762fd9abb6
maaluukii <malu.ki@tutanota.com>
2019-07-11 23:25:11 +0200

    trial

---
+++ rtcm2rinex_version.h
@@ -0,0 +1,30 @@
+#ifndef VERSION_rtcm2rinex_H
+#define VERSION_rtcm2rinex_H
+
+	//Date Version Types
+	static const char DATE[] = "11";
+	static const char MONTH[] = "07";
+	static const char YEAR[] = "2019";
+	static const char UBUNTU_VERSION_STYLE[] =  "19.07";
+	
+	//Software Status
+	static const char STATUS[] =  "Alpha";
+	static const char STATUS_SHORT[] =  "a";
+	
+	//Standard Version Type
+	static const long MAJOR  = 1;
+	static const long MINOR  = 0;
+	static const long BUILD  = 15;
+	static const long REVISION  = 64;
+	
+	//Miscellaneous Version Types
+	static const long BUILDS_COUNT  = 19;
+	#define RC_FILEVERSION 1,0,15,64
+	#define RC_FILEVERSION_STRING "1, 0, 15, 64\0"
+	static const char FULLVERSION_STRING [] = "1.0.15.64";
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	static const long BUILD_HISTORY  = 15;
+	
+
+#endif //VERSION_rtcm2rinex_H
